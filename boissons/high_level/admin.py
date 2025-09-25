0ad328ffe582c608cae8740d5e7058ac9d4bf47a
Merouane Bahri <merouanebhari1@gmail.com>
2025-09-25 16:11:54 +0200

    merouane

--- boissons/high_level/admin.py
+++ boissons/high_level/admin.py
@@ -1,9 +1,18 @@
+from django.db import admin
+from . import models
+
 # Register your models here.
-# admin.site.register(models.MatierePremiere)
-# admin.site.register(models.QuantiteMatierePremiere)
-# admin.site.register(models.Metier)
-# admin.site.register(models.Localisation)
-# admin.site.register(models.Energie)
-# admin.site.register(models.DebitEnergie)
-# admin.site.register(models.MatierePremiere)
-# admin.site.register(models.QuantiteMatierePremiere)
+admin.site.register(models.MatierePremiere)
+admin.site.register(models.QuantiteMatierePremiere)
+admin.site.register(models.Metier)
+admin.site.register(models.Localisation)
+admin.site.register(models.Energie)
+admin.site.register(models.DebitEnergie)
+admin.site.register(models.MatierePremiere)
+admin.site.register(models.ApprovisionnementMatierePremiere)
+admin.site.register(models.Local)
+admin.site.register(models.Produit)
+admin.site.register(models.UtilisationMatierePremiere)
+admin.site.register(models.RessourceHumaine)
+admin.site.register(models.Machine)
+admin.site.register(models.Fabrication)
