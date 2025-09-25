b6a62b2dfce5e70f757382de9220efb68824c341
Merouane Bahri <merouanebhari1@gmail.com>
2025-09-25 16:00:00 +0200

    merouane

--- boissons/high_level/admin.py
+++ boissons/high_level/admin.py
@@ -1,1 +1,9 @@
 # Register your models here.
+# admin.site.register(models.MatierePremiere)
+# admin.site.register(models.QuantiteMatierePremiere)
+# admin.site.register(models.Metier)
+# admin.site.register(models.Localisation)
+# admin.site.register(models.Energie)
+# admin.site.register(models.DebitEnergie)
+# admin.site.register(models.MatierePremiere)
+# admin.site.register(models.QuantiteMatierePremiere)
--- boissons/high_level/models.py
+++ boissons/high_level/models.py
@@ -1,1 +1,121 @@
+from django.db import models
 # Create your models here.
+# models.IntegerField()
+# models.CharField(max_length=100)
+# models.ManyToManyField(Machine)
+# models.ForeignKey(Local, on_delete=models.PROTECT,# blank=True, null=True, related_name="+",)
+
+
+class Metier(models.Model):
+    nom = models.CharField(max_length=100)
+    remuneration = models.IntegerField()
+
+
+class MatierePremiere(models.Model):
+    nom = models.CharField(max_length=100)
+    stock = models.IntegerField()
+    emprise = models.IntegerField()
+
+
+class QuantiteMatierePremiere(models.Model):
+    quantite = models.IntegerField()
+    matiere_premiere = models.ForeignKey(
+        MatierePremiere,
+        on_delete=models.PROTECT,
+    )
+
+
+class Meta:
+    abstract = True
+
+
+class Localisation(models.Model):
+    nom = models.CharField(max_length=100)
+    taxes = stock = models.IntegerField()
+    prix_m2 = stock = models.IntegerField()
+
+
+class Energie(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField()
+    localisation = models.ForeignKey(
+        Localisation,
+        on_delete=models.PROTECT,
+    )
+
+
+class DebitEnergie(models.Model):
+    debit = models.IntegerField()
+    energie = models.ForeignKey(
+        Energie,
+        on_delete=models.PROTECT,
+    )
+
+
+class ApprovisionnementMatierePremiere(QuantiteMatierePremiere):
+    localisation = models.ForeignKey(
+        Localisation,
+        on_delete=models.PROTECT,
+    )
+    prix_unitaire = models.IntegerField()
+    delais = models.IntegerField()
+
+
+class Local(models.Model):
+    nom = models.CharField(max_length=100)
+    localisation = models.ForeignKey(
+        Localisation,
+        on_delete=models.PROTECT,
+    )
+    surface = models.IntegerField()
+
+
+class Produit(models.Moedl):
+    nom = models.CharField(max_length=100)
+    prix_de_ventre = models.IntegerField()
+    quantite = models.IntegerField()
+    emprise = models.IntegerField()
+    local = models.ForeignKey(
+        Local,
+        on_delete=models.PROTECT,
+    )
+
+
+class UtilisationMatierePremiere(models.Model):
+    pass
+
+
+class RessourceHumaine(models.Model):
+    metier = models.ForeignKey(
+        Metier,
+        on_delete=models.PROTECT,
+    )
+    quantite = models.IntegerField()
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=100)
+    prix_achat = models.IntegerField()
+    cout_maintenance = models.IntegerField()
+    operateurs = models.ManyToManyField(RessourceHumaine)
+    debit = models.IntegerField()
+    surface = models.IntegerField()
+    debit_energie = models.ForeignKey(
+        DebitEnergie,
+        on_delete=models.PROTECT,
+    )
+    taux_utilisation = models.IntegerField()
+    local = models.ForeignKey(
+        Local,
+        on_delete=models.PROTECT,
+    )
+
+
+class Fabrication(models.Model):
+    produit = models.ForeignKey(
+        Produit,
+        on_delete=models.PROTECT,
+    )
+    utilisation_matiere_premiere = models.ManyToManyField(UtilisationMatierePremiere)
+    machines = models.ManyToManyField(Machine)
+    ressources_humaines = models.ManyToManyField(RessourceHumaine)
